aed2dc1c6a5f2eb41e8ff49ff7963ce4a9fa2de9
ampats04 <jeremyandyampatin@gmail.com>
2023-10-11 04:46:07 +0800

    Component(Footer): Partial Footer Component

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,7 +3,12 @@ import React from 'react';
 
 //Style Imports
 import './styles.css';
-import { Box } from './styles';
+import { Box, 
+        Container,
+        Heading,
+        Column,
+        Row,
+         } from './styles';
 
 //Asset Imports
 
@@ -17,8 +22,31 @@ export default function Footer(){
 
     return (
       <Box>
-
-
+        <Container>
+            <Row>
+                <Column>
+                    <Heading>About Us</Heading>
+                    <span>DreamTech elevates</span>
+                    <span>business by providing</span>
+                    <span>customized POS solutions.</span>
+                </Column>
+                <Column>
+                    <Heading>Contact Us</Heading>
+                    <span>Cebu City</span>
+                    <span>dreamtech@gmail.com</span>
+                    <span>09999999999</span>
+                </Column>
+                <Column>
+                    <Heading>Follow Us</Heading>
+                    <span><a href = "facebook.com">Facebook</a></span>
+                    <span><a href = "instagram.com">Instagram</a></span>
+                </Column>
+            </Row>
+            <Column>
+            <span> 2023 DREAMTECH.| PRIVACY POLICY</span>
+            </Column>
+        </Container>
+       
       </Box>
         
     )
