d426c8daf0e44cace475cd413c79345825ad0e5c
ampats04 <jeremyandyampatin@gmail.com>
2023-10-12 01:19:55 +0800

    Component(footer): Added new Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,15 +3,14 @@ import React from 'react';
 
 //Style Imports
 import './styles.css';
-import  { Box, 
-        Container,
-        Heading,
-        Column,
-        Row,
-        } from './styles';
 
 //Asset Imports
-
+import fb from '../../assets/fb.png';
+import ig from '../../assets/ig.png';
+import phone from '../../assets/phone.png';
+import email from '../../assets/email.png';
+import map from '../../assets/map.png';
+import copyright from '../../assets/copyright.png';
 /**
  * @author ampats04 (Jeremy Andy F. Ampatin)
  * Footer Component
@@ -20,34 +19,47 @@ import  { Box,
 
 export default function Footer(){
 
+    const year = new Date().getFullYear();
+
     return (
-      <Box>
-        <Container>
-            <Row>
-                <Column>
-                    <Heading>About Us</Heading>
-                    <span>DreamTech elevates</span>
-                    <span>business by providing</span>
-                    <span>customized POS solutions.</span>
-                </Column>
-                <Column>
-                    <Heading>Contact Us</Heading>
-                    <span>Cebu City</span>
-                    <span>dreamtech@gmail.com</span>
-                    <span>09999999999</span>
-                </Column>
-                <Column>
-                    <Heading>Follow Us</Heading>
-                    <span><a href = "facebook.com">Facebook</a></span>
-                    <span><a href = "instagram.com">Instagram</a></span>
-                </Column>
-            </Row>
-            <Column>
-            <span> 2023 DREAMTECH.| PRIVACY POLICY</span>
-            </Column>
-        </Container>
-       
-      </Box>
-        
-    )
+    <React.Fragment>
+        <div className = 'footer'>
+            <div className = 'sb_footer section_padding'>
+                <div className = 'sb_footer-links'>
+                    <div className = 'sb_footer-links-div'>
+                        <h3>About Us</h3>  
+                            <p>DreamTech elevates</p>
+                            <p>businesses by providing</p>
+                            <p>customized POS soluitions.</p>
+                    </div>
+
+                    <div className = 'sb_footer-links-div'>
+                        <h3>Contact Us</h3>  
+                        
+                            <p><img src = {map} alt = '' className = 'icon'/> Cebu City</p>
+                            <p><img src = {email} alt = '' className = 'icon'/> dreamtech@gmail.com</p>
+                            <p><img src = {phone} alt = '' className = 'icon'/> 099999999</p>
+                    </div>
+
+                    <div className = 'sb_footer-links-div'>
+                        <h3>Follow Us</h3>  
+                      
+                        <div className = 'socialmedia'>
+                            <p><a href = 'https://www.facebook.com'><img src = {fb} alt ='' /></a></p>
+                            <p><a href = 'https://www.instagram.com'><img src = {ig} alt ='' /></a></p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div className = 'sb_footer-below'>
+            <div className = 'sb_footer-copyright'>
+                <img src = {copyright} alt = '' className = 'icon-credits'/> 
+                <p>{year} DREAMTECH | PRIVACY POLICY </p>
+            </div>
+        </div>
+    </React.Fragment>
+    );
 }
