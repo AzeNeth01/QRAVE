7aba170858e512cbd379b883b2c8dd62d12497df
Daavvss <david.brigoli@module-zero.com>
2023-10-13 23:27:07 +0800

    Changes in SolutionToClient

--- src/pages/solutionToClient/solutionToClient.js
+++ src/pages/solutionToClient/solutionToClient.js
@@ -14,8 +14,8 @@ function solutionToClient() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold Poppins text-violet-500 my-4">SOLUTIONS</h1>
-      <p className="text-2xl font-Poppins mt-3">Web-based application for Point-of-Sales and Ordering Systems</p>
-      <p className="text-2xl font-Poppins italic text-almostGray mt-3">
+      <p className="text-2xl font-Poppins text-gray-500 mt-3">Web-based application for Point-of-Sales and Ordering Systems</p>
+      <p className="text-2xl font-Poppins italic text-gray-500 mt-3">
         We offer the QRave, a web-based application for point-of-sales for easy ordering and tracking sales, item history, purchases, and more.
       </p>
 
@@ -27,19 +27,24 @@ function solutionToClient() {
       </div>
 
       <div className="mx-4 sm:mx-24 md:mx-96">
-        <h2 className="text-3xl font-bold Poppins text-violet-500 mt-16">What Our Clients Say</h2>
-        <div className="flex flex-col sm:flex-row items-center justify-center relative">
-        <img src= {business} alt="Client Logo" className="w-full sm:w-40 md:w-48" />
-          <p className="text-2xl font-poppins mx-4 sm:mx-10">
-            “My business’s POS was created by DreamTech in 2018. The process involved planning, conceptualizing, and designing and rebranding my the POS in a way that is functional and efficient. I am so grateful to DreamTech for their efforts in providing me a POS system that is fit for my business.”
-            - ROMY PAREDES, ANTOJITOS MEXICANOS
-          </p>
-        </div>
-      </div>
+        <h2 className="text-3xl font-bold Poppins text-violet-500 mt-16">WHAT OUR CLIENT SAYS</h2>
+        <div class=" mx-auto p-5" style={{maxwidth: '100rem'}}>
+  <div class="row g-0">
+    <div class="col-md-4">
+    <img src={business} alt="Client Logo" class="img-fluid w-75 w-sm-65 w-md-65" />
+    </div>
+    <div class="col-md-7 d-flex align-items-center">
+      <p class="text-2xl mx-auto p-1 font-poppins card-text text-gray-500">
+        “My business’s POS was created by DreamTech in 2018. The process involved planning, conceptualizing, and designing and rebranding my the POS in a way that is functional and efficient. I am so grateful to DreamTech for their efforts in providing me a POS system that is fit for my business.”<br />- ROMY PAREDES, ANTOJITOS MEXICANOS
+      </p>
+    </div>
+  </div>
+</div>
+</div>
       <div>
         <Carousel />
-        
-      </div>
+        </div>
+      
     
     </div>
     
