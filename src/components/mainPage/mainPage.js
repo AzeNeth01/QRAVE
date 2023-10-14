904baf5330a002b8c0c1baa7915a4702195e3e2d
June-Azeneth <junesuco2001@gmail.com>
2023-10-14 12:49:41 +0800

    Edited file

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -4,10 +4,10 @@ import React from 'react';
 //Named Imports
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
-import Hero from '../../pages/hero/Hero';
-import SolutionToClient from '../../pages/solutionToClient/SolutionToClient';
-import Goals from '../../pages/goals/Goals';
-import WhoWeAre from '../../pages/hero/whoWeAre';
+import Hero from '../../pages/hero_section/Hero';
+import SolutionToClient from '../../pages/solutionToClient_section/SolutionToClient';
+import Goals from '../../pages/goals_section/Goals';
+import WhoWeAre from '../../pages/hero_section/whoWeAre';
 
 //Style Imports
 import '../mainPage/styles.css'
