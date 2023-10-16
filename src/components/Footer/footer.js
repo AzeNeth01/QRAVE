03122acd345d0c48588c966d46457dd4f73af60c
ampats04 <jeremyandyampatin@gmail.com>
2023-10-16 09:52:41 +0800

    style(footer): adjusted footer width and height

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -26,35 +26,38 @@ export default function Footer() {
 
         <div>
             <div class='row-12 footer-container'>
-                <div className='col'>
+                <div className='col text-center'>
                     <h3>About us</h3>
                     <p>DreamTech elevates businesses by providing customized POS Solutions</p>
                 </div>
 
-                <div className='col'>
+                <div className='col text-center'>
                     <h3>Contact Us</h3>
-                    <div className='inner-cont center'>
-                        <img src={location} alt="" className='icon' />
-                        <p>Cebu City</p>
-                    </div>
-                    <div className='inner-cont center'>
-                        <img src={email} alt="" className='icon' />
-                        <p>dreamtech@gmail.com</p>
-                    </div>
-                    <div className='inner-cont center'>
-                        <img src={phone} alt="" className='icon' />
-                        <p>+63 999 9999 999</p>
-                    </div>
+                 
+                      
+                        <p>  <img src={location} alt="" className='icon' />Cebu City</p>
+                  
+                  
+                       
+                        <p > <img src={email} alt="" className='icon ' />dreamtech@gmail.com</p>
+                   
+                    
+                      
+                        <p>  <img src={phone} alt="" className='icon' />+63 999 9999 999</p>
+                  
 
                 </div>
 
-                <div className='col'>
+                <div className='col text-center'>
                     <h3>Follow Us</h3>
                     <a href='www.facebook.com'><img src={fb} /></a>
                     <a href='www.instagram.com'><img src={ig} /></a>
                 </div>
+                
             </div>
-            <p className='copy-right'>© 2023 DREAMTECH. | PRIVACY POLICY</p>
+          <div className='copy-right'> <p className='copy-right '>© 2023 DREAMTECH. | PRIVACY POLICY</p></div>
         </div>
+        
+        
     );
 }
